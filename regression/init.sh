4fa3d8129e021bdfef342c71ef53e04383e5f048
Josef 'Jeff' Sipek <jsipek@cs.sunysb.edu>
2006-12-28 21:36:57 -0500

    Update/cleanup regression test for init
    Signed-off-by: Josef 'Jeff' Sipek <jsipek@cs.sunysb.edu>

--- regression/init.sh
+++ regression/init.sh
@@ -7,34 +7,10 @@ source scaffold
 function expected_files
 {
 	cat << DONE
-d .
-d ./.git
-d ./.git/refs
-d ./.git/refs/heads
-d ./.git/refs/tags
-d ./.git/branches
-d ./.git/remotes
-f ./.git/description
-d ./.git/hooks
-f ./.git/hooks/applypatch-msg
-f ./.git/hooks/post-update
-f ./.git/hooks/commit-msg
-f ./.git/hooks/update
-f ./.git/hooks/pre-applypatch
-f ./.git/hooks/pre-commit
-f ./.git/hooks/post-commit
-f ./.git/hooks/pre-rebase
-d ./.git/info
-f ./.git/info/exclude
-d ./.git/objects
-d ./.git/objects/pack
-d ./.git/objects/info
-f ./.git/HEAD
-f ./.git/config
-d ./.git/patches
-d ./.git/patches/master
-f ./.git/patches/master/series
-f ./.git/patches/master/status
+d .git/patches
+d .git/patches/master
+f .git/patches/master/series
+f .git/patches/master/status
 DONE
 }
 
@@ -42,6 +18,7 @@ DONE
 empty_repo
 cd $REPODIR
 gq-init
-expected_files | verify_repo
+expected_files | verify_repo .git/patches
 
 complete_test
+
