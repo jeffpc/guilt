02ddba36df6c6ddee0c25756de796d776c394578
Josef 'Jeff' Sipek <jsipek@cs.sunysb.edu>
2007-02-04 00:05:49 -0500

    regression: Test guilt-init on non-master branch
    Signed-off-by: Josef 'Jeff' Sipek <jsipek@cs.sunysb.edu>

--- regression/010-init.sh
+++ regression/010-init.sh
@@ -4,7 +4,7 @@
 
 source scaffold
 
-function expected_files
+function expected_files_master
 {
 	cat << DONE
 d .git/patches
@@ -14,15 +14,41 @@ f .git/patches/master/status
 DONE
 }
 
+function expected_files_other
+{
+	expected_files_master
+	cat << DONE
+d .git/patches/other
+f .git/patches/other/series
+f .git/patches/other/status
+DONE
+}
+
 # the test itself
 empty_repo
 cd $REPODIR
-guilt-init
-expected_files | verify_repo .git/patches
-echo -n "[create] "
 
-shouldfail guilt-init
-echo -n "[exists] "
+# make sure we have an index to work with
+echo "abc" > def
+git-add def 2> /dev/null > /dev/null
+git-commit -m "foo" 2> /dev/null > /dev/null
+
+branches="master other"
+
+for b in $branches
+do
+	if [ "$b" != "master" ]; then
+		git-branch $b
+		git-checkout $b 2> /dev/null
+	fi
+
+	guilt-init
+	expected_files_$b | verify_repo .git/patches
+	echo -n "[$b:create] "
+
+	shouldfail guilt-init
+	echo -n "[$b:exists] "
+done
 
 complete_test
 
