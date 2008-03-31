524372efbd255b519bdc4390f3b7363aea9ca513
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2008-03-31 16:20:52 -0400

    regression: 011 should test guilt-commit as well
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- regression/t-011.sh
+++ regression/t-011.sh
@@ -8,7 +8,7 @@ source $REG_DIR/scaffold
 
 cmd setup_git_repo
 
-tests="guilt-add guilt-applied guilt-branch guilt-delete guilt-diff guilt-export guilt-files guilt-fold guilt-fork guilt-graph guilt-header guilt-import guilt-import-commit guilt-new guilt-next guilt-pop guilt-prev guilt-push guilt-rebase guilt-refresh guilt-rm guilt-series guilt-status guilt-top guilt-unapplied"
+tests="guilt-add guilt-applied guilt-branch guilt-commit guilt-delete guilt-diff guilt-export guilt-files guilt-fold guilt-fork guilt-graph guilt-header guilt-import guilt-import-commit guilt-new guilt-next guilt-pop guilt-prev guilt-push guilt-rebase guilt-refresh guilt-rm guilt-series guilt-status guilt-top guilt-unapplied"
 for t in $tests; do
 	shouldfail $t
 done
